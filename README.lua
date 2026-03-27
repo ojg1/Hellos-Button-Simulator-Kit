5e8057a41828add82a6fd75e79da09b9665174d2
OrangeJuiceGuy1 <ericlin1253@gmail.com>
2026-03-26 21:37:48 -0400

    README basic instructions, update later

--- README.lua
+++ README.lua
@@ -1,2 +1,22 @@
 
-Not finished
+--[[
+# Welcome to H|DBS Kit V1.1
+
+## diff:
+H|DBS Kit V1.1
+--removed EternityNum because of precision errors
+--rewrote InteractiveActivation (pre: TagHandler.lua) for clarity and debugging
+
+H|DBS Kit V1.2
+--added README.md (not located in actual kit) and README.lua (roblox)
+--new formatting for InteractiveAction
+--posted on GitHub
+
+For beginners:
+Move folder "ReplicatedStorage" in HDBS Kit V1.2 into game.ReplicatedStorage and unpack
+Move folder "ServerScriptService" in HDBS Kit V1.2 into game.ServerScriptService and unpack
+Move folder "ServerStorage" in HDBS Kit V1.2 into game.ServerStorage and unpack
+
+README.md and README.luau not fully wrote. 
+
+# Please write README and kit suggestions preferably in https://www.github.com/ojg1/Hello-s-Difficulty-Button-Simulator-Kit/issues.
